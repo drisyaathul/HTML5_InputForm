b3c785b7d9c31c4e5355af4bca4579501b0b2edc
drisyaathul <drisyak809@gmail.com>
2023-06-18 01:47:23 +0800

    Added: Valid Email Address

--- simpleForm.js
+++ simpleForm.js
@@ -7,6 +7,17 @@ if (nameRegex.test(text.value))
  else 
     textError.textContent = "Name is Incorrect";
 });
+
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function() {
+let emailRegex = RegExp(('^[0-9a-z]+([+.-]([a-z0-9]+))*[@][a-z]+[.][a-z]{2,4}([.][a-z]{2,4})?$'));
+if (emailRegex.test(email.value)) 
+    emailError.textContent = "";
+ else 
+    emailError.textContent = "Invalid Email Address";
+});
+
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-ouput');
 output.textContent = salary.value;
